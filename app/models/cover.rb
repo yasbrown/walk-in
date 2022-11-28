abe6ffeb147b00fbf77d0dfa631e559546328ed0
yasmin brown <yasminbrown6@hotmail.com>
2022-11-28 17:28:42 +0000

    setting associations + validations

--- app/models/cover.rb
+++ app/models/cover.rb
@@ -1,3 +1,4 @@
 class Cover < ApplicationRecord
   belongs_to :restaurant
+  has_many :bookings, dependent: :destroy
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :covers, :favourite_restaurants, dependent: :destroy
+
+  validates :name, presence: true
+  validates :rating, presence: true
+  validates :address, presence: true
+  validates :total_seats_available, presence: true
+  validates :cuisine, presence: true
+
 end
