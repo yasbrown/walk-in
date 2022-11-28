ddb179d9302c728048d8e8391366cce9e9ab358d
John Romero <romerojohn1012@gmail.com>
2022-11-28 17:58:33 +0000

    make references

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_171818) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_175727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_171818) do
     t.bigint "cover_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["cover_id"], name: "index_bookings_on_cover_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "covers", force: :cascade do |t|
@@ -50,7 +36,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_171818) do
     t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["restaurant_id"], name: "index_favourite_restaurants_on_restaurant_id"
+    t.index ["user_id"], name: "index_favourite_restaurants_on_user_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -63,7 +51,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_171818) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "bookings", "covers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "covers", "restaurants"
   add_foreign_key "favourite_restaurants", "restaurants"
+  add_foreign_key "favourite_restaurants", "users"
 end
