6f818104e7a31f169211373d95453deb464d510c
Lydia Ratto <lydiaratto@gmail.com>
2022-11-29 15:33:55 +0000

    updated routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants, only: [:index, :show]
-
-  resources :covers, only: [:show] do
-    resources :bookings, only: %i[new create show]
+  resources :users, only: [:show]
+  resources :restaurants, only: %i[index show] do
+    resources :favourite_restaurants, only: %i[create destroy]
+    resources :covers, only: [:create]
   end
-  resources :bookings, only: %i[update delete]
 
+  resources :covers, only: [] do
+    resources :slots, only: %i[index create]
+  end
+  resources :slots, only: [] do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:destroy] do
+    get "confirmation", to: "bookings#confirm", as: :confirmation
+  end
 end
---
+++ test/controllers/favourite_restaurants_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FavouriteRestaurantsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
