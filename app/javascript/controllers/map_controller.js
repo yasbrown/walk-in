868059b3311693e8397875bca81b3adbf9dfd376
yasmin brown <yasminbrown6@hotmail.com>
2022-11-30 15:36:15 +0000

    fit map to markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,6 +27,8 @@ export default class extends Controller {
   }
 
   #fitMapToMarkers() {
-
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
