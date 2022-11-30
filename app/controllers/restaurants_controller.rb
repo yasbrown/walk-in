a872fc64de9018470bba1c88843333e2d094f86b
francisco goicoechea <frangoico97@gmail.com>
2022-11-30 17:10:02 +0000

    delete pg gem

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,7 @@ class RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:query].present?
-      @restaurants = Restaurant.global_search(params[:query])
-      if @restaurants.nil?
-        @Restaurants = Restaurant.all
-      end
-    else
-      @restaurants = Restaurant.all
-    end
+   @restaurants = Restaurant.all
  end
 
   def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -16,16 +16,5 @@ class Restaurant < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  include PgSearch::Model
-  pg_search_scope :global_search,
-  against: [ :address, :opening_time, :closing_time],
-  associated_against: {
-    covers: [:seats]
-  },
-  using: {
-    tsearch: { prefix: true }
-  }
-
-
 
 end
