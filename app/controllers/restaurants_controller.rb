881df10180c3c45cbef2cfc9e9a7a013c11bbdb1
John Romero <romerojohn1012@gmail.com>
2022-11-30 18:14:04 +0000

    Home page not done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,26 +2,26 @@ class RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-
     if params.present?
       @restaurants = Restaurant.where("address ILIKE ? AND opening_time <= ? AND closing_time >= ?", params.dig(:restaurant, :address), params.dig(:restaurant, :opening_time), params.dig(:restaurant, :closing_time))
       if @restaurants.empty?
         @restaurants = Restaurant.all
       end
     else
+      puts "sorry we are still not there.."
       @restaurants = Restaurant.all
     end
-  end
 
-    if params[:query].present?
-      @restaurants = Restaurant.where.(address: params[:query])
-      @markers = @restaurants.geocoded.map do |restaurant|
-        {
-          lat: restaurant.latitude,
-          lng: restaurant.longitude
-        }
-      end
-    else
+    # address = params.dig(:restaurant, :address)
+    # if address.present?
+    #   @restaurants = Restaurant.where.(address: address)
+    #   @markers = @restaurants.geocoded.map do |restaurant|
+    #     {
+    #       lat: restaurant.latitude,
+    #       lng: restaurant.longitude
+    #     }
+    #   end
+    # else
       @restaurants = Restaurant.all
       @markers = @restaurants.geocoded.map do |restaurant|
         {
@@ -29,7 +29,7 @@ class RestaurantsController < ApplicationController
           lng: restaurant.longitude
         }
       end
-    end
+    # end
  end
 
 
