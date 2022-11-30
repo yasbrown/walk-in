ba9f4dc5a2c04f4f219946e4e38d23bc49a8f715
yasmin brown <yasminbrown6@hotmail.com>
2022-11-30 15:23:05 +0000

    footer fix

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,8 +4,20 @@ class RestaurantsController < ApplicationController
   def index
     if params[:query].present?
       @restaurants = Restaurant.where.(address: params[:query])
+      @markers = @restaurants.geocoded.map do |restaurant|
+        {
+          lat: restaurant.latitude,
+          lng: restaurant.longitude
+        }
+      end
     else
       @restaurants = Restaurant.all
+      @markers = @restaurants.geocoded.map do |restaurant|
+        {
+          lat: restaurant.latitude,
+          lng: restaurant.longitude
+        }
+      end
     end
  end
 
