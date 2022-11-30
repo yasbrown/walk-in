3096665c2428039a6ecebb072bc93f6582e13d92
John Romero <romerojohn1012@gmail.com>
2022-11-30 15:42:17 +0000

    home page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    if params.present?
+      @restaurants = Restaurant.where("address ILIKE ? AND opening_time <= ? AND closing_time >= ?", params.dig(:restaurant, :address), params.dig(:restaurant, :opening_time), params.dig(:restaurant, :closing_time))
+      if @restaurants.empty?
+        @restaurants = Restaurant.all
+      end
+    else
+      @restaurants = Restaurant.all
+    end
   end
 
   def show
