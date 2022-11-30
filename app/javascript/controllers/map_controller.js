b036f8eec3404bb18c66035b5a3b0a86daa80862
yasmin brown <yasminbrown6@hotmail.com>
2022-11-30 15:34:35 +0000

    added markers to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,15 +13,20 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
-    })
-    // this.#addMarkersToMap()
+    });
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
   }
 
-  // #addMarkersToMap() {
-  //   this.markersValue.forEach((marker) => {
-  //     new mapboxgl.Marker()
-  //       .setLngLat([ marker.lng, marker.lat ])
-  //       .addTo(this.map)
-  //   })
-  // }
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+
+  }
 }
