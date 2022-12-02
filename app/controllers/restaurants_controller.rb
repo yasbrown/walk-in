9d283973d3748cee8a2af5f224315767ac9749d4
francisco goicoechea <frangoico97@gmail.com>
2022-12-02 11:29:40 +0000

    index filter not work

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,14 +4,19 @@ class RestaurantsController < ApplicationController
   def index
     if params.present?
       query = "address ILIKE ? AND opening_time <= ? AND closing_time >= ? AND total_seats_available >= ?"
-      address = params.dig(:restaurant, :address)
-      opening_time = params.dig(:restaurant, :opening_time)
-      closing_time = params.dig(:restaurant, :closing_time)
-      total_seats_available = params.dig(:restaurant, :total_seats_available)
-      @restaurants = Restaurant.where(query, "%#{address}%", opening_time, closing_time, total_seats_available)
-      if @restaurants.empty?
-        @restaurants = Restaurant.all
+      @address = params.dig(:restaurant, :address)
+      @opening_time = params.dig(:restaurant, :opening_time)
+      @closing_time = params.dig(:restaurant, :closing_time)
+      @total_seats_available = params.dig(:restaurant, :total_seats_available)
+      query2 = "address ILIKE ? AND opening_time <= ? AND closing_time >= ? AND total_seats_available >= ? AND cuisine ? AND rating >= ?"
+      @cuisine = params.dig(:restaurant, :cuisine)
+      @rating = params.dig(:restaurant, :rating)
+      @restaurants = Restaurant.where(query, "%#{@address}%", @opening_time, @closing_time, @total_seats_available)
+      if !@cuisine.nil? && !@rating.nil?
+        query2 = "address ILIKE ? AND opening_time <= ? AND closing_time >= ? AND total_seats_available >= ? AND cuisine ILIKE ? AND rating >= ?"
+        @filtered_restaurants = @restaurants.where(query2, "%#{@address}%", @opening_time, @closing_time, @total_seats_available, @cuisine, @rating)
       end
+      @restaurants = Restaurant.all if @restaurants.empty?
     else
       puts "sorry we are still not there.."
       @restaurants = Restaurant.all
