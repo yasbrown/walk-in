4b69031cc82b991f2224ff53cacd72af660b4abc
yasmin brown <yasminbrown6@hotmail.com>
2022-12-02 12:14:12 +0000

    file

--- db/seeds.rb
+++ db/seeds.rb
@@ -97,8 +97,8 @@ puts "Building new restaurants"
     date: Date.new(2022, 12, 9),
     total_seats_available: rand(100..150)
   )
-  # file = URI.open("#{restaurant_image.sample}")
-  file = URI.open("https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&w=1000&q=80")
+  file = URI.open("#{restaurant_image.sample}")
+  # file = URI.open("https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&w=1000&q=80")
   restaurant.photo.attach(io: file, filename: "restaurant.png", content_type: "image/png")
   restaurant.save
 
