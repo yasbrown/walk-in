6476ab0091e1253127ac788e42e2cd0af1a3e1e2
yasmin brown <yasminbrown6@hotmail.com>
2022-12-04 16:32:22 +0000

    removed footer from home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,31 +11,48 @@ require "nokogiri"
 # #   Character.create(name: "Luke", movie: movies.first)
 
 url = "https://www.timeout.com/london/restaurants/best-restaurants-in-london"
-html = URI.open(url)
+html = URI.open(url).read
 doc = Nokogiri::HTML(html)
+
 restaurants_names = doc.search(".articleContent h3")
+restautants_description = doc.search("._summary_1p2xe_21 p")
+restaurants_cuisines = doc.search("._text_163gl_28")
+restaurants_urls = doc.search("._title_1p2xe_9 a")
+restaurant_url = restaurants_urls[0]["href"]
 
+restaurant_show_html = URI.open(restaurant_url)
+restaurant_doc = Nokogiri::HTML(restaurant_show_html)
 
-# postcode = %i[SW98FG W38QL SE186EQ NW29QN W30LG NW108GZ SW23AS N228XY N99HH E163DN N32AG W66SS NW34SD E49AN W52DL W27DG
-#               SE16AP SE280NW SW194NS SW100BP E174DQ E29ET NW106EU N179UZ N128SD E84FL SE207BL SW198EY SW129JL SW66QS
-#               N10AJ NW96AB N52RT SE137DU NW24EX E143TJ SE228SU NW95ES E130AP E201GA E96QE N135UJ E111BH N89LE SW22DP
-#               N178JA SE156NE E113JA NW35JP N14NN SW73NX N11SB NW51RN SW66SP N90FP N211HE SW195AS SE13RQ N128RW SE165SZ
-#               N146QN W51HW SE15EL E97QG NW31TP SW162XD E10NQ NW25RZ SE114RR NW110HS SW64HF NW11LT N154FX SE135AF N77JL
-#               N127NJ SW33LH SW120LA E18QW SW116AR SW184HA W106LW W73PQ SE163LU SW47EP N79RR N15NU NW71FR N77PL NW37RS
-#               NW72QP SE228LA SE279QY N52TL N225EP N166PT N43JJ N182XE SE12LY NW99JJ E18NF N65NW SE270RS E15RN SW151AP
-#               N99PE NW41LL E105GT SE171LP SE155FA SE231BT SW196EN W127JG NW87AB W73QH SE265BE E10RD SE116HT SE136DH
-#               W26DZ NW74RG SW46AJ N176JY E79PX W127FR N31JN N29BA NW67BW SE280EL SE57AZ SE193HE NW80RF E50BQ N179RW
-#               N160ER N195RP SW66XQ SE228DL N28JX NW25PJ N154FT N88RL W43HB SE18TN SW40DU SW155HH N212AT N29PS NW72PE
-#               SW97ET E140FF SE156BU SW62EW SW178JB SE29AP SW185JZ SW191TX N43DJ SE62HP NW88PQ]
+# *-- RETURNS RESTAURANT NAME BUT WITH LEADING WHITESPACE --*
+names_array = []
+restaurants_names.take(20).each do |restaurants_name|
+  names = restaurants_name.text.strip.split(/\d+.\W/)
+  names_array << names[1]
+end
+# p names_array
 
-# review_content = ["Dinner was amazing! We got here around 9pm on a Tuesday night and the wait was over an hour. A little surprised by that but the time went by pretty quick. They brought out some lemonade while we were waiting which was nice. Food itself was great and the atmosphere is amazing as well",
-#                   "Amazing experience. Visited for a lunch for my birthday and the staff made it super special with attentive, helpful and genuine service. Looking forward to going back.",
-#                   "What a place!! Had breakfast here and absolutely loved it. Highly recommended!!",
-#                   "Lovely restaurant, nice crowd and delicious food. Will definitely come back soon!",
-#                   "Amazing food to share and good drink menu with creative cocktails and natural wine selection. Love the at atmosphere and super nice staff.",
-#                   "Lovely small place with a small and delicious, innovative menu. Great service, much love goes into the food."]
+# *-- RETURNS RESTAURANT CUISINE --*
+cuisine_array = []
+restaurants_cuisines.take(60).each do |restaurants_cuisine|
+  cuisine_array << restaurants_cuisine.text.strip
+end
+# p cuisine_array
 
-# restaurant_type = %i[Italian Indian French Vegetarian Vegan Thai Japanese]
+# *-- RETURNS RESTAURANT DESCRIPTION & PRICE --*
+description_array = []
+restautants_description.take(60).each do |description|
+  description_array << description.text.strip
+end
+p description_array
+
+# *-- RETURNS ADDRESS, POSTCODE, WEBSITE, TRANSPORT & MENU LINK --*
+details_array =[]
+details = restaurant_doc.search("._list_1fhdc_5 dd")
+details.each do |detail|
+  details_array << detail.text.strip
+end
+# p details_array[0]
+
 
 # restaurant_image = ["https://res.cloudinary.com/dft14camn/image/upload/v1669898682/qiyikkxbiguukfrwtuch.jpg",
 #                     "https://res.cloudinary.com/dft14camn/image/upload/v1669898677/hpxsndb6rciucmq0kiuq.jpg",
@@ -90,15 +107,13 @@ puts "Building new restaurants"
 20.times do
   result = Geocoder.search(postcode.sample)
   address = result.first.display_name
-
+  n = 0
+  c = 2
 
   restaurant = Restaurant.create!(
-    name: restaurants_names.take(30).each do |restaurants_name|
-      names = restaurants_name.text.strip.split(".")
-      p names[1].gsub(/\s+/, '')
-      end,
+    name: names_array[n += 1],
     rating: rand(3.0...5.0).round(2),
-    cuisine: restaurant_type.sample,
+    cuisine: cuisine_array[c += 3],
     description: Faker::Restaurant.description,
     address: address,
     price: rand(1..3),
