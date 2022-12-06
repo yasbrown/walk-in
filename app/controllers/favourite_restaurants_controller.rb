ef3ce9cdde82da43587117fdbf7e1eb7815690ab
francisco goicoechea <frangoico97@gmail.com>
2022-12-06 18:09:35 +0000

    heart design show page

--- app/controllers/favourite_restaurants_controller.rb
+++ app/controllers/favourite_restaurants_controller.rb
@@ -1,11 +1,14 @@
 class FavouriteRestaurantsController < ApplicationController
   def create
+    if !current_user
+      redirect_to new_user_registration_path
+    end
     @restaurant = Restaurant.find(params[:restaurant_id])
     @favourite_restaurant = FavouriteRestaurant.new
     @favourite_restaurant.restaurant = @restaurant
     @favourite_restaurant.user = current_user
     @favourite_restaurant.save!
-    redirect_to root_path
+    redirect_to user_path(current_user)
   end
 
   def destroy
