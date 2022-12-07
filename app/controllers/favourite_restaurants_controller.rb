2032742f23ae0a22110f022d6c4ad80cd8a7873c
yasmin brown <yasminbrown6@hotmail.com>
2022-12-07 11:19:38 +0000

    css user page

--- app/controllers/favourite_restaurants_controller.rb
+++ app/controllers/favourite_restaurants_controller.rb
@@ -5,7 +5,6 @@ class FavouriteRestaurantsController < ApplicationController
     @favourite_restaurant.restaurant = @restaurant
     @favourite_restaurant.user = current_user
     @favourite_restaurant.save!
-    redirect_to root_path
   end
 
   def destroy
